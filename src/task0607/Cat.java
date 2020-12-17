7a581958759b505991d085de8930bed7c1ef9c97
Larin Aleksei <larin_ai00@mail.ru>
2020-12-18 00:04:51 +0300

    decided

--- src/task0607/Cat.java
+++ src/task0607/Cat.java
@@ -1,4 +1,14 @@
 package task0607;
 
 public class Cat {
+    //напишите тут ваш код
+    public static int catCount = 0;
+
+    public Cat () {
+        catCount++;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
